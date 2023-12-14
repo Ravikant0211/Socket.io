867b1e6918afffc4a5574dc312f7bc82b0ac550e
Ravi kant <Ravikant02111@gmail.com>
2023-12-14 11:54:00 +0530

    creating chat application

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http')
+const path = require('path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`server running at http://localhost:${PORT}`);
+})
